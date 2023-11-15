c26b5ec4c55414d8e163c955f32fb33d77be8f50
Leechanbe <143692842+Leechanbe@users.noreply.github.com>
2023-11-15 22:18:49 +0900

    Merge branch 'develop' of https://github.com/yuuun03/java_bobPlus2023.git into develop 수정

--- src/user/UserInfoDetail.java
+++ src/user/UserInfoDetail.java
@@ -18,7 +18,7 @@ public class UserInfoDetail {
 	private int birth = 0;
 	
 	//---알러지
-	public Vector<> allergy = new Vector<> ();
+	public Vector<String> allergy = new Vector<String> ();
 	
 	//---배송지 주소
 	protected String address = "";
@@ -26,10 +26,55 @@ public class UserInfoDetail {
 	//---본인인증
 	protected boolean idVerification = false;
 	
-	//---
+	//---목록: 장바구니, 찜, 구매
+	protected Vector<String> cartList = new Vector<String> ();
+	protected Vector<String> likeList = new Vector<String> ();
+	protected Vector<String> buyList = new Vector<String> ();
+	
 	//---생일 쿠폰
 	protected int birthCoupon = 0;
 	
+	// 멤버 메소드
 	// 생성자
 	public UserInfoDetail() {}
+	public UserInfoDetail(String name, String id, String email, String password, 
+			long phoneNum, int birth, Vector<String> allergy, String address, 
+			boolean idVerification, Vector<String> cartList, Vector<String> liketList,
+			Vector<String> buytList, int birthCoupon) {
+		//---회원 이름
+		this.name = name;
+		
+		//---아이디 비번
+		this.id = id;
+		this.password = password;
+		
+		//---이메일, 전화번호, 생일
+		this.email = email;
+		this.phoneNum = phoneNum;
+		this.birth = birth;
+		
+		//---알러지
+		this.allergy = allergy;
+		
+		//---배송지 주소
+		this.address = address;
+		
+		//---본인인증
+		this.idVerification = idVerification;
+		
+		//---목록: 장바구니, 찜, 구매
+		this.cartList = cartList;
+		this.likeList = likeList;
+		this.buyList = buyList;
+		
+		//---생일 쿠폰
+		this.birthCoupon = birthCoupon;
+	}
+	
+	// 접근자
+	
+	// 설정자
+	
+	
+	
 }
