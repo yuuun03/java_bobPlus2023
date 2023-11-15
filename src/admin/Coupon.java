c9c228c4183b58cc564a7a2db7f91e6ba68593dc
P13Xm4 <150218326+P13Xm4@users.noreply.github.com>
2023-11-15 23:05:29 +0900

    Coupon 변수 추가

--- src/admin/Coupon.java
+++ src/admin/Coupon.java
@@ -1,3 +1,14 @@
 package admin;
 
-public class Coupon{}
+public class Coupon{
+
+  static int couponNum;
+  static String couponName;
+  static double couponDisRate;
+  static double couponDue;
+  static int couponDisPrice;
+  
+  public static void main (String[] args){
+
+  }
+}
