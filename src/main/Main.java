6508a7db7ed193124c317bb3fe70564094404c17
yscho <103940294+yuuun03@users.noreply.github.com>
2023-11-15 22:20:44 +0900

    MainFrame 생성.(Main은 삭제할 클래스)

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,16 @@
 package main;
 
-public class Main {
+import javax.swing.*;
 
+public class Main extends JFrame{
+	public Main() { //메인페이지 프레임
+		setTitle("밥심+"); //제목 설정
+		setSize(1920, 1080); //윈도우 사이즈 1920, 1080 고정.
+		setVisible(true); // 프레임 출력
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
-
 }
