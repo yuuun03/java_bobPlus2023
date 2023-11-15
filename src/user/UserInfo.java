b91db8a8ba71a649509633c3578fd3cd0ea1fc75
Leechanbe <143692842+Leechanbe@users.noreply.github.com>
2023-11-15 21:09:18 +0900

    UserInfo 함수 추가

--- src/user/UserInfo.java
+++ src/user/UserInfo.java
@@ -1,5 +1,16 @@
 package user;
 
 public class UserInfo {
-
+	public String getName() {}
+	public Long getPhoneNum() {}
+	private void isThatYou(String password) {}
+	private void addAllergy(String allergy) {}
+	private void delAllergy(String allergy) {}
+	public void setPw(String password) {}
+	public void setName(String name) {}
+	public void setEmail(String email) {}
+	public void setBirth(int birth) {}
+	void showUserInfo() {}
+	public String showDeliveryInfo() {}
+	
 }
