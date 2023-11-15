0cb4088adde9b951c1f4ae60ec7c66a87525c570
Leechanbe <143692842+Leechanbe@users.noreply.github.com>
2023-11-15 21:50:15 +0900

    추가

--- src/user/UserInfo.java
+++ src/user/UserInfo.java
@@ -1,8 +1,41 @@
 package user;
 
+import java.util.Vector;
+
 public class UserInfo {
+	//---멤버 변수
+	
+	//---회원 이름
+	private String name = "";
+	
+	//---아이디 비번
+	private String id = "";
+	private String password = "";
+	
+	//---이메일, 전화번호, 생일
+	private String email = "";
+	private long phoneNum = 0;
+	private int birth = 0;
+	
+	//---알러지
+	public Vector<> allergy = new Vector<> ();
+	
+	//---배송지 주소
+	protected String address = "";
+	
+	//---본인인증
+	protected boolean idVerification = false;
+	
+	//---
+	//---생일 쿠폰
+	protected int birthCoupon = 0;
+	
+	// 생성자
+	public UserInfo() {}
+	
+	
 	public String getName() {}
-	public Long getPhoneNum() {}
+	public long getPhoneNum() {}
 	private void isThatYou(String password) {}
 	private void addAllergy(String allergy) {}
 	private void delAllergy(String allergy) {}
@@ -12,5 +45,4 @@ public class UserInfo {
 	public void setBirth(int birth) {}
 	void showUserInfo() {}
 	public String showDeliveryInfo() {}
-	
 }
