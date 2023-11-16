36c9f6a9193ab247a1fa1abaa1d3779aa68e23a4
P13Xm4 <150218326+P13Xm4@users.noreply.github.com>
2023-11-16 09:42:56 +0900

    Update UserInfo.java

--- src/user/UserInfo.java
+++ src/user/UserInfo.java
@@ -17,7 +17,7 @@ public class UserInfo {
 	public void setPw(String password) {}
 	public void setName(String name) {}
 	public void setEmail(String email) {}
-	static void setBirth(int birth) {}
+	public static void setBirth(int birth) {}
 	void showUserInfo() {}
 	public String showDeliveryInfo() {}
 }
