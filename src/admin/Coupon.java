f432bde156ee21592a53b3aa7e77019caa2773c3
P13Xm4 <150218326+P13Xm4@users.noreply.github.com>
2023-11-16 10:30:24 +0900

    Update Coupon.java

--- src/admin/Coupon.java
+++ src/admin/Coupon.java
@@ -1,5 +1,9 @@
 package admin;
 
+import java.util.Date;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Coupon{
 
   static int couponNum;
@@ -8,7 +12,20 @@ public class Coupon{
   static double couponDue;
   static int couponDisPrice;
   
-  public static void main (String[] args){
 
+  
+  public int getBirth() {
+	  return birth;
+  }
+  
+  public static void main (String[] args){
+	  
+	 
+	  
+	  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+	  
+	  
+	  LocalDate date = LocalDate.now();
+	  LocalDate date2 = LocalDate.of(birth); 
   }
 }
