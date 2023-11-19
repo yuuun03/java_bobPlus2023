38edc5d769d9d13ea4b5da338e7b2e1a7239ade3
Leechanbe <143692842+Leechanbe@users.noreply.github.com>
2023-11-19 22:56:42 +0900

    마이페이지 작성 중

--- src/main/MyPage.java
+++ src/main/MyPage.java
@@ -12,14 +12,6 @@ public class MyPage extends JFrame{
 		
 		// 마이페이지 프레임 구현 내용
 		public MyPage() {
-			//---아래쪽 레이블 : 설정 중 . . .
-			//JButton popularGoods = new
-			//화면 기본 설정 - End
-			setSize(1920, 1080); //윈도우 사이즈 1920, 1080 고정.
-			setVisible(true); // 프레임 출력
 			
-			//화면 기본 설정 - Start
-			setTitle("밥심+"); //제목 설정
-			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		}
 }
