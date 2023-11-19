b22c476eef46118970eb6a3bb22e5ecfaabaf49d
yscho <103940294+yuuun03@users.noreply.github.com>
2023-11-20 00:09:45 +0900

    아래 버튼들 추가 완료. 이벤트는 구현중에 있음.

--- src/main/MainFrame.java
+++ src/main/MainFrame.java
@@ -25,17 +25,18 @@ public class MainFrame extends JFrame{
 		JButton weekTop10Goods = new JButton("금주의 TOP 10");
 		JButton checkAttendance = new JButton("출석 체크");
 		JButton couponPoint = new JButton("쿠폰/포인트");
-		JButton community = new JButton("커뮤니티");
+		JButton communityU = new JButton("커뮤니티");
 		JButton newMonthGoods = new JButton("이달의 신상품");
 		
 		JButton[] upLabel = {newHotGoods, weekTop10Goods, checkAttendance,
-				couponPoint, community, newMonthGoods};
+				couponPoint, communityU, newMonthGoods};
 
 		for(int i = 0; i < 6 ; i++) {
 			upLabel[i].setSize(190,43); //사이즈 설정
 			upLabel[i].setLocation(58 + 245 * i, 160); //위치 설정
 			/*위쪽 라벨 폰트 설정
 			 * G마켓 산스 TTF Medium체, 사이즈 17
+			 * 깃허브와 연동해보고 안되면 해당 주석 사용.
 			*/
 			//upLabel[i].setFont(new Font("G마켓 산스 TTF Medium", Font.CENTER_BASELINE, 17));
 			mainPane.add(upLabel[i]); // 메인 페이지에 버튼 추가
@@ -45,7 +46,49 @@ public class MainFrame extends JFrame{
 		}
 		
 		//---아래쪽 레이블 : 설정 중 . . .
-		//JButton popularGoods = new
+		Image popImg = new ImageIcon("D:\\eclipseCode\\BobPlus2023\\src\\graphics\\images\\popular.png").getImage();
+		popImg = popImg.getScaledInstance(130, 130, Image.SCALE_SMOOTH);
+		ImageIcon popularIcon = new ImageIcon(popImg);
+		JButton popularGoods = new JButton("인기 상품", popularIcon);
+		
+		Image todImg = new ImageIcon("D:\\eclipseCode\\BobPlus2023\\src\\graphics\\images\\today_s_pick.png").getImage();
+		todImg = todImg.getScaledInstance(130, 130, Image.SCALE_SMOOTH);
+		ImageIcon todayIcon = new ImageIcon(todImg);
+		JButton todaySPick = new JButton("오늘 뭐 먹지?", todayIcon);
+		
+		Image salImg = new ImageIcon("D:\\eclipseCode\\BobPlus2023\\src\\graphics\\images\\sale.png").getImage();
+		salImg = salImg.getScaledInstance(130, 130, Image.SCALE_SMOOTH);
+		ImageIcon saleIcon = new ImageIcon(salImg);
+		JButton sale = new JButton("지금 할인 중", saleIcon);
+		
+		Image hitImg = new ImageIcon("D:\\eclipseCode\\BobPlus2023\\src\\graphics\\images\\hit.png").getImage();
+		hitImg = hitImg.getScaledInstance(130, 130, Image.SCALE_SMOOTH);
+		ImageIcon hitIcon = new ImageIcon(hitImg);
+		JButton hit = new JButton("인기 급상승", hitIcon);
+		
+		Image comImg = new ImageIcon("D:\\eclipseCode\\BobPlus2023\\src\\graphics\\images\\community.png").getImage();
+		comImg = comImg.getScaledInstance(130, 130, Image.SCALE_SMOOTH);
+		ImageIcon communityIcon = new ImageIcon(comImg);
+		JButton communityD = new JButton("커뮤니티", communityIcon);
+		
+		JButton[] downLabel = {popularGoods, todaySPick, sale, hit, communityD};
+		
+		for(int j = 0; j < 5; j++) {
+			//폰트 위치 설정
+			downLabel[j].setVerticalTextPosition(JButton.BOTTOM); //수직 설정
+			downLabel[j].setHorizontalTextPosition(JButton.CENTER);//수평 설정
+			
+			//형태 설정
+			downLabel[j].setBorderPainted(false); //외곽선 없앰
+			downLabel[j].setContentAreaFilled(false); //배경색 없앰	
+			downLabel[j].setSize(150, 150); //사이즈 설정
+			
+			//버튼 위치 설정
+			downLabel[j].setLocation(200 + 250 * j, 600); //위치 설정
+			mainPane.add(downLabel[j]);
+		}
+		
+		
 		//화면 기본 설정 - End
 		setSize(1920, 1080); //윈도우 사이즈 1920, 1080 고정.
 		setVisible(true); // 프레임 출력
