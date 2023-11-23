a0240b33e970b3bec27770f006b7139426f0513a
java_bobPlus2023 <yhjlilly@naver.com>
2023-11-23 10:23:07 +0900

    객체 생성

--- src/goods/SearchResult.java
+++ src/goods/SearchResult.java
@@ -3,9 +3,14 @@ package goods;
 import java.awt.event.*;
 import java.awt.*;
 import javax.swing.*;
+import admin.MasterGoods;
+import admin.Product;
 
 
 public class SearchResult{
+	public void printproduct() {
+		MasterGoods mg = new MasterGoods();
+	}
 	//MasterGoods.java에서 pList 받아오기
 	//받아온 pList와 검색어(Search.java) 비교 >> 8개 띄우기
 	//필터 정보 받아오기(SearchFilter.java) >> 상품의 알러지 정보와 비교하여 상품 8개 띄우기
