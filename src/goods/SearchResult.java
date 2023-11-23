82178638d84ccc36dbc2743304437da72b098cc7
java_bobPlus2023 <yhjlilly@naver.com>
2023-11-23 09:51:45 +0900

    구현을 위한 주석 추가

--- src/goods/SearchResult.java
+++ src/goods/SearchResult.java
@@ -6,5 +6,7 @@ import javax.swing.*;
 
 
 public class SearchResult{
-	
+	//MasterGoods.java에서 pList 받아오기
+	//받아온 pList와 검색어(Search.java) 비교 >> 8개 띄우기
+	//필터 정보 받아오기(SearchFilter.java) >> 상품의 알러지 정보와 비교하여 상품 8개 띄우기
 }
