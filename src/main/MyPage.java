2d34351a2bd28d8c51e3f4f37d34d404a45a032e
Leechanbe <143692842+Leechanbe@users.noreply.github.com>
2023-11-24 00:28:38 +0900

    밥심+로고 띄우기

--- src/main/MyPage.java
+++ src/main/MyPage.java
@@ -1,6 +1,8 @@
 package main;
 
 import javax.swing.*;
+
+import main.MainFrame.MainActionListener;
 import user.SingUp;
 
 import java.awt.*;
@@ -29,6 +31,22 @@ public class MyPage extends JFrame{
 			Container mypagePane = getContentPane(); //컨텐트 팬 얻어오기. 여기서 awt 사용.
 			mypagePane.setLayout(null); //배치관리자 없음 : 개발자 자유 배치
 			
+			//로고, 검색창, 위쪽 레이블, 로그인, 장바구니 등 기본 패널 추가
+			CommonPanel df = new CommonPanel(); //패널 객체 생성
+			add(df.commonPanel); //패널 추가
+			
+			//---액션 설정
+			df.login.addActionListener(new MainActionListener());
+			df.my.addActionListener(new MainActionListener()); //마이페이지
+			df.cart.addActionListener(new MainActionListener());
+			
+			df.newHotGoods.addActionListener(new MainActionListener());
+			df.weekTop10Goods.addActionListener(new MainActionListener());
+			df.checkAttendance.addActionListener(new MainActionListener());
+			df.couponPoint.addActionListener(new MainActionListener());
+			df.communityU.addActionListener(new MainActionListener());
+			df.newMonthGoods.addActionListener(new MainActionListener());
+			
 			JLabel mypagelogo = new JLabel("마이페이지");
 			mypagelogo.setLocation(260, 50);
 			mypagelogo.setSize(100,50);
@@ -49,5 +67,65 @@ public class MyPage extends JFrame{
 			setVisible(true); // 프레임 출력
 			
 			
+		}
+		//이벤트 처리 클래스들
+		class MainActionListener implements ActionListener{
+			//Action : 버튼 클릭 
+			public void actionPerformed(ActionEvent e) {
+				JButton bRefer = (JButton)e.getSource(); //사용자가 클릭한 버튼 알아내기
+				
+				//버튼 종류마다 이벤트 다르게 지정
+				switch(bRefer.getText()) {
+				
+				case "로그인": 
+					new LoginPage(); //로그인 페이지 전환
+					setVisible(false); //기존 페이지 안보이게 변경
+					break;
+				
+				case "MY": 
+					new MyPage();
+					setVisible(false);
+					break;
+					
+				case "인기 상품": case "지금 뜨는 상품" : case "금주의 TOP 10" :
+					/*인기상품, 지금뜨는 상품, 금주의 TOP10 클릭시
+					지금뜨는 상품과 금주의 TOP10은 인기 상품에 속해있는 원소긴 하나
+					이는 추후 구현 예정*/ 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				case "출석 체크": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				case "쿠폰/포인트": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+					
+				case "커뮤니티": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				case "이달의 신상품": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				case "오늘 뭐 먹지?": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+
+				case "지금 할인 중": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+
+				case "인기 급상승": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				default : //장바구니 클릭 시 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				}
+			}
 		}
 }
