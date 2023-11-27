6f2a7c54445dba4a7f791f479e8dd0812e95d705
java_bobPlus2023 <yhjlilly@naver.com>
2023-11-28 03:06:49 +0900

    CommonPanel이랑 겹치는 코드 주석처리 (액션만 남김)

--- src/goods/Search.java
+++ src/goods/Search.java
@@ -8,6 +8,19 @@ public class Search extends JFrame implements ActionListener{
 	private JButton searchButton;
 	private String goodsName;
 	
+	public void actionPerformed(ActionEvent e) { //액션
+		if(e.getSource() == searchButton) { //'검색'버튼 입력이 인식되면
+			goodsName = searchField.getText(); //입력되어있는 텍스트 받아오기
+		}
+	}
+	
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(()->{
+			new Search();
+		});
+	}
+	
+	/*
 	public void Search() {
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(600, 200);
@@ -22,17 +35,8 @@ public class Search extends JFrame implements ActionListener{
 		pan.add(searchButton);
 		
 		add(pan);
-		setVisible(true); //여기까지가 검색창 띄우는거 (CommonPanel이랑 겹침)
+		setVisible(true);
 	}
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == searchButton) { //'검색'버튼 입력이 인식되면
-			goodsName = searchField.getText(); //입력되어있는 텍스트 받아오기
-		}
-	}
+	*/ //검색창 띄우는거 (CommonPanel이랑 겹침)
 	
-	public static void main(String[] args) {
-		SwingUtilities.invokeLater(()->{
-			new Search();
-		});
-	}
 }
