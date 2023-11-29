08cc0e82de0d1cd347c6af6c9565c127f79f9b93
yscho <103940294+yuuun03@users.noreply.github.com>
2023-11-30 03:09:08 +0900

    검색 모두 구현 완료

--- src/goods/ShowOneGoods.java
+++ src/goods/ShowOneGoods.java
@@ -12,93 +12,277 @@ import main.*;
 import goods.*;
 import user.*;
 
-public class ShowOneGoods {
-	
-	public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            new ShowOneGoods(); // SearchResult 객체 생성
-        });
-    }
-	
+public class ShowOneGoods extends JFrame {
 	JPanel oneGoodsPannel = new JPanel();
+	private Thread th; //진동하는 스레드 구현 위함
 	
-	public ShowOneGoods() {
-		MasterGoods mg = new MasterGoods();
-		UserInfoDetail user = new UserInfoDetail();
+	public ShowOneGoods() {}
+	public ShowOneGoods(UserInfoDetail myUser, Vector<Product> pList, Product p) {
+		//화면 기본 설정 - Start
+		setTitle("밥심+"); //제목 설정
+		//---아이콘 설정
+		Toolkit kit = Toolkit.getDefaultToolkit();
+		Image img = kit.getImage("src/graphics/images/iconOnly.png");
+		setIconImage(img);
+				
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//종료 설정	
 		
-		Product oneProduct = new Product();
-		//oneProduct = SearchResult의 이름 클릭으로부터 받아온 Product 정보
+		setLayout(null);
+				
+		//로고, 검색창, 위쪽 레이블, 로그인, 장바구니 등 기본 패널 추가
+		CommonPanel df = new CommonPanel(); //패널 객체 생성
+		add(df.commonPanel); //패널 추가
+				
+		//---액션 설정
+		df.mainIL.addMouseListener(new MouseAdapter() {
+			public void mouseReleased(MouseEvent e) {
+				new MainFrame(myUser, pList);
+			}
+		});
+				
+		df.login.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new LoginPage(myUser, pList); //로그인 페이지 전환
+				dispose(); //기존 페이지 안보이게 변경
+			}});
+				
+		df.my.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new MyPage(myUser, pList); //로그인 페이지 전환
+				dispose(); //기존 페이지 안보이게 변경
+			}});
+					
+		df.cart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new Cart(myUser, pList); //로그인 페이지 전환
+				dispose(); //기존 페이지 안보이게 변경
+			}});
+				
+		df.search.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String pName = e.getActionCommand();
+				//상품 객체까지 매개변수로 받아와야하나 지금 일시적으로 미지정함.
+				new SearchResult(myUser, pList, pName);
+				dispose();
+			}
+		});
+			
+		df.newHotGoods.addActionListener(new MainActionListener());
+		df.weekTop10Goods.addActionListener(new MainActionListener());
+		df.checkAttendance.addActionListener(new MainActionListener());
+		df.couponPoint.addActionListener(new MainActionListener());
+		df.communityU.addActionListener(new MainActionListener());
+		df.newMonthGoods.addActionListener(new MainActionListener());
 		
-		for(String CU:user.getAllergy()) { //알러지 정보 필터링
-			if(oneProduct.getContainAllergy().contains(CU)) {
+		//단일 상품 보기 상세 파트 구현
+
+		for(String CU:myUser.getAllergy()) { //알러지 정보 필터링
+			if(p.getContainAllergy().contains(CU)) {
 				Image warn = new ImageIcon("src/graphics/images/warning.png").getImage();
 				warn = warn.getScaledInstance(100, 100, Image.SCALE_SMOOTH);
 				ImageIcon iconWarn = new ImageIcon(warn);
 				JLabel warnAl = new JLabel(iconWarn);
-				warnAl.setSize(30,30); //사이즈 다시 잡아야함
-				warnAl.setLocation(20, 20); //위치 다시 잡아야함
+				warnAl.setSize(100,100); //사이즈 다시 잡아야함
+				warnAl.setLocation(1370, 333); //위치 다시 잡아야함
 				oneGoodsPannel.add(warnAl);
 				//진동하는거 추가...? (교재 참고)
 			}
 		}
-		displayOneProduct(oneGoodsPannel, oneProduct);
 		
-		JButton productDetail = new JButton("상품 상세 정보 더보기");
+		//상품 정보 출력
+		displayOneProduct(oneGoodsPannel, p);
+		
+		//버튼
+		Font bFont = new Font("G마켓 산스 TTF BOLD", Font.CENTER_BASELINE, 30);
+		
+		JButton productDetail = new JButton("상품 상세 정보");
+		productDetail.addActionListener(new OneProductActionListener());
+		productDetail.setBounds(580, 453, 300, 90);
+		productDetail.setFont(bFont);
+		productDetail.setBackground(new Color(255, 206, 90));
+		
+		
 		JButton showReview = new JButton("리뷰");
-		JButton inCart = new JButton("장바구니 담기");
-		JButton goBuy = new JButton("구매하기");
+		showReview.addActionListener(new OneProductActionListener());
+		showReview.setBounds(890, 453, 170, 90);
+		showReview.setFont(bFont);
+		showReview.setBackground(new Color(255, 206, 90));
 		
+		JButton inCart = new JButton("장바구니");
+		inCart.addActionListener(new OneProductActionListener());
+		inCart.setBounds(1070, 453, 220, 90);
+		inCart.setFont(bFont);
+		inCart.setBackground(new Color(200, 228, 137));
+		
+		JButton goBuy = new JButton("구매");
+		goBuy.addActionListener(new OneProductActionListener());
+		goBuy.setBounds(1300, 453, 170, 90);
+		goBuy.setFont(bFont);
+		goBuy.setBackground(new Color(243, 138, 117));
+		
+		oneGoodsPannel.add(productDetail);
+		oneGoodsPannel.add(showReview);
+		oneGoodsPannel.add(inCart);
+		oneGoodsPannel.add(goBuy);
+		
+		/*
 		df.productDetail.addActionListener(new OneProductActionListener());
 		df.showReview.addActionListener(new OneProductActionListener());
 		df.inCart.addActionListener(new OneProductActionListener());
 		df.goBuy.addActionListener(new OneProductActionListener());
+		*/
 		
 		oneGoodsPannel.setLayout(null);
 		
 		oneGoodsPannel.setBackground(Color.white);
-		oneGoodsPannel.setBounds(240, 240, 1260, 700);
+		oneGoodsPannel.setBounds(0, 203, 1920, 877);
 		
-		mainPanel.add(oneGoodsPannel);
-		
-		setTitle("One Product");
+		add(oneGoodsPannel);
+		//setTitle("One Product");
 		setSize(1920, 1080); // 적절한 크기로 설정
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true); // 프레임을 보이도록 설정
 	}
 	
 	private void displayOneProduct(JPanel oneGoodsPannel, Product product) { //상품 패널을 표시하는 함수
-		if(product != null) { //상품 정보가 null이 아니면
-			JLabel nameLabel = new JLabel(product.getName());
-				
-			Image productImg = new ImageIcon(product.getImage()).getImage(); //사진 이렇게 불러오는 게 맞는지...?
-			ImageIcon productIcon = new ImageIcon(productImg);
+		if(product != null) { //상품 정보가 null이 아니면	
+			//이미지 설정
+			Image img = new ImageIcon(product.getImage()).getImage();
+			img = img.getScaledInstance(520, 520, Image.SCALE_SMOOTH);
+			ImageIcon productIcon = new ImageIcon(img);
 			JLabel proImg = new JLabel(productIcon);
-			proImg.setSize(500,500); //사진 사이즈 설정
-				
+			
+			proImg.setBounds(40, 30, 520, 520); //사진 사이즈 설정
+			
+			//이름 설정
+			JLabel nameLabel = new JLabel(product.getName());
+			nameLabel.setBounds(580, 25, 1000, 100);
+			nameLabel.setFont(new Font("G마켓 산스 TTF BOLD", Font.CENTER_BASELINE, 80));
+			
+			//할인율 설정
+			double disrate = product.getProductDisRate();
+			JLabel disrateLabel = new JLabel(Double.toString(disrate)+"%");
+			disrateLabel.setBounds(580, 115, 1000, 100);
+			disrateLabel.setFont(new Font("G마켓 산스 TTF BOLD", Font.CENTER_BASELINE, 45));
+			disrateLabel.setForeground(Color.red);
+			
+			//가격 설정
+			int price = product.getPrice();
+			double disPrice = price *((100-disrate)/100);
+			JLabel disPriceLabel = new JLabel(Double.toString(disPrice) + " 원");
+			disPriceLabel.setBounds(750, 115, 1000, 100);
+			disPriceLabel.setFont(new Font("G마켓 산스 TTF BOLD", Font.CENTER_BASELINE, 45));
+			
+			JLabel priceLabel = new JLabel("원가 " + Integer.toString(price) + "원");
+			priceLabel.setBounds( (1000 + (Double.toString(disPrice) + " 원").length()) , 125, 1000, 100);
+			priceLabel.setFont(new Font("G마켓 산스 TTF Light", Font.CENTER_BASELINE, 15));
+			priceLabel.setForeground(Color.gray);
+			
+			//1인분당 가격 설정
+			double onePrice = product.getOnePersonPrice();
+			JLabel onePriceLabel = new JLabel ("1인분당 : " + Double.toString(onePrice)+"원");
+			onePriceLabel.setBounds(580, 170, 1000, 100);
+			onePriceLabel.setFont(new Font("G마켓 산스 TTF Medium", Font.CENTER_BASELINE, 30));
+			
+			//별점 설정
 			double star = product.getProductStar();
 			JLabel starLabel = new JLabel ("별점:" + Double.toString(star));
-				
-			int price = product.getPrice();
-			JLabel priceLabel = new JLabel(Integer.toString(price));
-				
-			double onePrice = product.getOnePersonPrice();
-			JLabel onePriceLabel = new JLabel ("1인분당 " + Double.toString(onePrice)+"원");
-				
-			double disrate = product.getProductDisRate();
-			JLabel disrateLabel = new JLabel(Double.toHexString(disrate)+"%");
-				
+			starLabel.setBounds(580, 220, 1000, 100);
+			starLabel.setFont(new Font("G마켓 산스 TTF Medium", Font.CENTER_BASELINE, 30));
+			
+			//조리기구 정보
+			Vector<String> cuBundle = product.getCookingUtensils();
+			
+			String uList = "";
+			for(int i = 0 ; i < cuBundle.size(); i++ ) {
+				uList += cuBundle.get(i);
+				if(i == cuBundle.size()-1) {uList += " ";}
+				else {uList +=", ";}
+			}
+			JLabel cUtensilLabel = new JLabel("조리 기구 : " + uList);
+			cUtensilLabel.setBounds(580, 270, 1000, 100);
+			cUtensilLabel.setFont(new Font("G마켓 산스 TTF Medium", Font.CENTER_BASELINE, 30));
+			
+			//알러지 정보
+            Vector<String> alBundle = product.getContainAllergy();
+			
+			String alList = "";
+			for(int i = 0 ; i < alBundle.size(); i++ ) {
+				alList += alBundle.get(i);
+				if(i == alBundle.size()-1) {alList += " ";}
+				else {alList +=", ";}
+			}
+			
+			JLabel allergyLabel = new JLabel("알레르기 유발 식재료 : " + alList);
+			allergyLabel.setBounds(580, 320, 1000, 100);
+			allergyLabel.setFont(new Font("G마켓 산스 TTF Medium", Font.CENTER_BASELINE, 30));
+			
 			oneGoodsPannel.add(nameLabel);
 			oneGoodsPannel.add(proImg);
 			oneGoodsPannel.add(starLabel);
+			oneGoodsPannel.add(disPriceLabel);
 			oneGoodsPannel.add(priceLabel);
 			oneGoodsPannel.add(onePriceLabel);
 			oneGoodsPannel.add(disrateLabel);
+			oneGoodsPannel.add(cUtensilLabel);
+			oneGoodsPannel.add(allergyLabel);
 		}
 		else {
 			JLabel noProduct = new JLabel("상품이 조회되지 않습니다.");
 			oneGoodsPannel.add(noProduct);
 		}
 	}
+	
+	//이벤트 처리 클래스들
+		class MainActionListener implements ActionListener{
+			//Action : 버튼 클릭 
+			public void actionPerformed(ActionEvent e) {
+				JButton bRefer = (JButton)e.getSource(); //사용자가 클릭한 버튼 알아내기
+				
+				//버튼 종류마다 이벤트 다르게 지정
+				switch(bRefer.getText()) {
+					
+				case "인기 상품": case "지금 뜨는 상품" : case "금주의 TOP 10" :
+					/*인기상품, 지금뜨는 상품, 금주의 TOP10 클릭시
+					지금뜨는 상품과 금주의 TOP10은 인기 상품에 속해있는 원소긴 하나
+					이는 추후 구현 예정*/
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				case "출석 체크": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				case "쿠폰/포인트": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+					
+				case "커뮤니티": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				case "이달의 신상품": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				case "오늘 뭐 먹지?": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+
+				case "지금 할인 중": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+
+				case "인기 급상승": 
+					JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
+					break;
+				
+				default : //장바구니 클릭 시 
+					break;
+				}
+			}
+		}
+	
 	class OneProductActionListener implements ActionListener{
 		//Action : 버튼 클릭 
 		public void actionPerformed(ActionEvent e) {
@@ -107,7 +291,7 @@ public class ShowOneGoods {
 			//버튼 종류마다 이벤트 다르게 지정
 			switch(bRefer.getText()) {
 						
-			case "상품 상세 정보 더보기":
+			case "상품 상세 정보":
 				JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
 				break;
 					
@@ -115,12 +299,12 @@ public class ShowOneGoods {
 				JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
 				break;
 					
-			case "장바구니 담기": 
+			case "장바구니": 
 				JOptionPane.showMessageDialog(null, "장바구니에 담겼습니다.");
 				//장바구니 기능 추가하는 경우 추가적인 코드 필요 (상품 객체 넘기는 코드 필요)
 				break;
 						
-			case "구매하기": 
+			case "구매": 
 				JOptionPane.showMessageDialog(null, "현재 기능 구현 중에 있습니다.");
 				break;
 			}
