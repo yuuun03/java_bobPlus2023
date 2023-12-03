2574865c07e247bc86b6b5005982edbc7f588e34
yscho <103940294+yuuun03@users.noreply.github.com>
2023-12-04 03:18:34 +0900

    필요없는 기능 삭제 및 함수 배치 변경

--- src/admin/MasterGoods.java
+++ src/admin/MasterGoods.java
@@ -11,6 +11,9 @@ public class MasterGoods {
 	//생성자
 	public MasterGoods() {} //기본 생성자
 	
+	public Vector<Product> getPList(){return this.pList;} //상품 리스트 getter
+	public void setPList(Vector<Product> newList){this.pList = newList;} //상품 리스트 setter
+	
 	//상품 추가(생성자 아님 주의!!!) //test상품 추가를 위해 public으로 변경
 	public void productAdd(String name, String image, double productStar,
 			int serving, int productCount, int price,
@@ -26,11 +29,9 @@ public class MasterGoods {
 		pList.add(p);
 	}
 	
-	public Vector<Product> getPList(){return this.pList;} //상품 리스트 getter
-	public void setPList(Vector<Product> newList){this.pList = newList;} //상품 리스트 setter
 	
 	//상품 삭제 : 이름 입력 받으면 삭제.
-	private void productDelete(String name) {
+	protected void productDelete(String name) {
 		if (pList.isEmpty()) {System.out.println("등록되어 있는 상품이 없습니다.");} //등록된 상품 자체가 없을 경우 경고문 출력
 		for(int i = 0; i < pList.size(); i++) {
 			//pList의 현재 인덱스에 있는 객체의 이름이 받아온 이름과 같을 경우
@@ -45,7 +46,7 @@ public class MasterGoods {
 	}
 	
 	//상품 리스트 전체 출력 //확인을 위해 public으로 변경
-	public void checkProduct() {
+	protected void checkProduct() {
 		//등록된 상품 자체가 없을 경우 경고문 출력
 		if (pList.isEmpty()) {System.out.println("등록되어 있는 상품이 없습니다.");} 
 		else { //출력
